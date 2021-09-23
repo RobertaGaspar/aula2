1f38d24d4e513d5fd564f6e680d4cb55b2c3fe49
Roberta Gaspar <roberta.gaspar@fatec.sp.gov.br>
2021-09-23 09:50:36 -0300

    Teste local Openfeign

--- src/main/kotlin/com/tutofox/aula2/UsuarioAula1.kt
+++ src/main/kotlin/com/tutofox/aula2/UsuarioAula1.kt
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 
-@FeignClient(name = "usuario", url = "https://localhost:8080/")
+@FeignClient(name = "usuario", url = "http://localhost:8080/")
 interface UsuarioAula1 {
 
     @RequestMapping(method = [RequestMethod.GET], value = ["/usuario"])
